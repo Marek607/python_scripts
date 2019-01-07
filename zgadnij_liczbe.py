fa2e1ca7a251080a3e2817ddeb74f0936df2146b
Marek607 <marek607@gmail.com>
2019-01-07 21:09:44 +0100

    Update zgadnij_liczbe.py

--- zgadnij_liczbe.py
+++ zgadnij_liczbe.py
@@ -6,11 +6,13 @@ def zobacz_pomoc(liczba_podana,liczba_wytypowana):
     elif liczba_podana < liczba_wytypowana:
         return print("Liczba jest za mała!")
 
-liczba_podana = 0
 liczba_max = 100
 liczba_wytypowana = int(random.randint(0,liczba_max))
-while liczba_podana != liczba_wytypowana:
-    zobacz_pomoc(liczba_podana,liczba_wytypowana)
+while True:
     liczba_podana = int(input("Zgadnij liczbę wymyśloną przez komputer w zakresie od 0 do {} : ".format(liczba_max)))
-
-print("Gratulacje, zgadłeś!")
+    if liczba_podana != liczba_wytypowana:
+        zobacz_pomoc(liczba_podana,liczba_wytypowana)
+        continue
+    else:
+        print("Gratulacje, zgadłeś!")
+        break
